c8063e20e2b09e537f9cde47cbba2152042b83c4
Svetlana <kotlyarova.sa@gmail.com>
2021-04-13 17:22:22 +0300

    my-script

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir tasks
+cd tasks
+touch task1.txt task2.txt
+ls -1 | sed "3,100d" > task3.txt
+cp task3.txt task4.txt
+echo "***task4.txt***"
+cat task4.txt
+echo "This is the best script ever!"
+ps -e > task1.txt
+grep "user" task1.txt > task2.txt
+
